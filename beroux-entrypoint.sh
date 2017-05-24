60e3848517529159722c47a06653f8550c11149b
Werner Beroux <werner@beroux.com>
2017-05-24 16:59:15 +0200

    Specifies the namespace explicitely.
    This makes it work also when running in the non `default` namespace.

--- beroux-entrypoint.sh
+++ beroux-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Auto-use Kubernetes service account:
 if [ -f /var/run/secrets/kubernetes.io/serviceaccount/token ]; then
-    kubectl config set-cluster default --server=https://kubernetes --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+    kubectl config set-cluster default --server=https://kubernetes.default.svc.cluster.local --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
     kubectl config set-credentials service-account --token="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
     kubectl config set-context default --cluster=default --user=service-account
     kubectl config use-context default
